11bd00401ea7f5b8d7197b6ceae808e80ab7efa8
stojanchevski <nikolastojancevski@yahoo.com>
2021-12-02 16:27:29 +0100

    Commit2.0MapsHeaderFooter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,15 @@ import { RecorderComponent } from './recorder/recorder.component';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { LocationsComponent } from './locations/locations.component';
 
+
 const routes:Routes=[
   {path:'',component:HomePageComponent},
   {path:'test',component:TestComponentComponent},
   {path:'recorder-article',component:RecorderComponent},
   {path:'donating-facts',component:DonatingFactsComponent},
   {path:'contact-us',component:ContactUsComponent},
-  {path:'locations',component:LocationsComponent}
+  {path:'locations',component:LocationsComponent},
+
   ]
 
 @NgModule({
